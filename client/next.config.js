1f5a68c204b99b2ad73b31158070d8bda51e6121
Anas <110332632+anasAS02@users.noreply.github.com>
2023-11-01 03:41:19 +0200

    .

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+
+// next.config.js
+const withImages = require('next-images');
+
+module.exports = withImages({
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+});
