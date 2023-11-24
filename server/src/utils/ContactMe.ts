548d91db163ac601b02bc6cd5129ee1014540dc1
Anas <110332632+anasAS02@users.noreply.github.com>
2023-11-24 22:30:29 +0200

    .

--- server/src/utils/ContactMe.ts
+++ server/src/utils/ContactMe.ts
@@ -1,43 +1,31 @@
 import { Request, Response, NextFunction } from 'express';
-import { asyncWrapper } from "../middlewares/asyncWrapper";
 import nodemailer from 'nodemailer';
-import { httpStatusText } from './httpStatusText';
-import AppError from './appError';
 
-const sendEmail = asyncWrapper(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { name, email, subject, message } = req.body;
-    
-    if(!name || !email || !subject || !message){
-      const error = new AppError('All fields are required', 401, httpStatusText.ERROR);
-      return next(error);
-    }
+const sendEmail = async(req: Request, res: Response, next: NextFunction) => {
+  const { name, email, subject, message } = req.body;
+	try {
+		const transporter = nodemailer.createTransport({
+			host: process.env.HOST,
+			service: process.env.SERVICE,
+			port: Number(process.env.EMAIL_PORT),
+			secure: Boolean(process.env.SECURE),
+			auth: {
+				user: process.env.USER,
+				pass: process.env.PASS,
+			},
+		});
 
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.EMAIL_USER, 
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
-    const mailOptions = {
-      from: email,
-      to: process.env.EMAIL_USER,
-      subject: `${subject} - ${name} | ${email}`,
-      text: message,
-    };
-
-    transporter.sendMail(mailOptions, (error: any, info: any) => {
-      if (error) {
-        console.error(error);
-        res.status(500).json({ status: httpStatusText.FAIL, message: 'Email not sent' });
-      } else {
-        console.log(`Email sent: ${info.response}`);
-        res.status(200).json({ status: httpStatusText.SUCCESS, message: 'Email sent successfully' });
-      }
-    });
-  }
-);
+		await transporter.sendMail({
+			from: email,
+			to: process.env.USER,
+			subject: subject,
+			text: `${name} - ${message}`,
+		});
+		console.log("Email sent successfully");
+	} catch (error) {
+		console.log("Email not sent!");
+		console.log(error);
+	}
+};
 
 export { sendEmail };
