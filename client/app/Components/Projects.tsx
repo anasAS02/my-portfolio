7a3a3a896d13ddfc35b14945eb564c7969707a0e
Anas <110332632+anasAS02@users.noreply.github.com>
2024-07-05 21:51:06 +0300

    Update projects section

--- client/app/Components/Projects.tsx
+++ client/app/Components/Projects.tsx
@@ -14,7 +14,7 @@ export default function Projects(){
     return(
         <section id='projects' className='opacity-25 duration-300 projects flex flex-col items-center justify-center gap-10 mt-16'>
             <h2 className='underline underline-offset-8 text-5xl max-md:text-2xl text-center'>My Projects</h2>
-            <div className='grid grid-cols-2 max-sm:grid-cols-1 gap-5 items-center justify-center p-10'>
+            <div className='flex flex-wrap gap-5 items-center justify-center p-10'>
                 {loading ?
                 <p className='text-5xl max-md:text-2xl absolute left-2/4 -translate-x-2/4 -translate-y-2/4 animate-pulse'>Loading ...</p>
                 :
@@ -23,7 +23,7 @@ export default function Projects(){
                         <div className='desc flex flex-col items-center'>
                             <Link target={"_blank"} href={`project/${project._id}`} className='p-3 duration-200 text-white text-sm rounded-xl bg-blue-500 hover:bg-blue-400'>View</Link>
                         </div>
-                        <img className='rounded-tr-3xl rounded-bl-3xl w-full' src={`${project.img}.png`} alt={project.title} />
+                        <img className='rounded-tr-3xl rounded-bl-3xl w-[250px]' src={`${project.img}.png`} alt={project.title} />
                     </span>
                 ))}
             </div>
