ba3709a45f20b4b275c181e776a7451fef6925b4
Anas <110332632+anasAS02@users.noreply.github.com>
2024-08-12 15:53:59 +0300

    Add download cv button

--- client/app/Components/Landing.tsx
+++ client/app/Components/Landing.tsx
@@ -1,16 +1,17 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAnglesDown } from '@fortawesome/free-solid-svg-icons';
 import { TypeAnimation } from 'react-type-animation';
+import Link from 'next/link';
 
 export default function Landing(){
     return(
         <main className='text-center mt-48 p-10 flex flex-col gap-52'>
-            <div className='text-3xl max-md:text-sm max-sm:text-base text-black dark:text-white'>
+            <div className='text-3xl max-md:text-sm max-sm:text-base text-black dark:text-white flex flex-col justify-center items-center gap-12 h-full'>
                 <TypeAnimation
                     sequence={[
-                        'I\'M Anas, a Frontend React Developer',
-                        1000,
-                        'I\'M Anas, a Frontend Next Developer',
+                        'I\'M Anas, ',
+                        300,
+                        'I\'M Anas, a Frontend Developer',
                         1000,
                     ]}
                     wrapper="span"
@@ -18,7 +19,13 @@ export default function Landing(){
                     style={{fontSize: '2em', display: 'inline-block'}}
                     repeat={Infinity}
                 />
-                <p className='mt-12 text-2xl max-md:text-sm'>I enjoy building new projects daily and constantly strive to learn more.</p>
+                <p className='text-2xl max-md:text-sm'>I enjoy building new projects daily and constantly strive to learn more.</p>
+                <Link href='https://drive.usercontent.google.com/download?id=1zN33NkCsdllMqdrORXE133xM9F2RAAzZ&export=download&authuser=0&confirm=t&uuid=0588b635-c47a-413a-bff5-8d840998ccce&at=APZUnTX52KQyG98-if5jhk-3eBMG:1723466269280'
+                className='flex items-center gap-3 duration-300 animate-bounce bg-blue-400 hover:bg-blue-300 text-black p-5 max-md:p-2 rounded-xl cursor-pointer'
+                >
+                    Download CV
+                    <FontAwesomeIcon icon={faAnglesDown} />
+                </Link>
             </div>
         </main>
     )
