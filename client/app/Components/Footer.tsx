b7dd0e24ef504586fffe10bfd203520d21953994
Anas <110332632+anasAS02@users.noreply.github.com>
2024-08-21 15:25:01 +0300

    Update Footer

--- client/app/Components/Footer.tsx
+++ client/app/Components/Footer.tsx
@@ -4,6 +4,9 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 
 export default function Footer(){
+    const date = new Date();
+    const year = date.getFullYear();
+
     return(
         <footer id='footer' className='bg-slate-100 dark:bg-zinc-900 text-black dark:text-white text-center p-5 mt-14 flex flex-col items-center justify-center gap-5 w-full'>
                 <span className='flex gap-6'>
@@ -17,7 +20,7 @@ export default function Footer(){
                         <FontAwesomeIcon icon={faEnvelope} className='text-4xl max-md:text-2xl text-black dark:text-white duration-200 hover:text-red-600 dark:hover:text-red-600' />
                     </Link>
                 </span>
-            <p>Copyrights reserved to <span className='font-bold text-red-500'>Anas</span> - 2023&copy;</p>
+            <p>Copyrights reserved to <span className='font-bold text-red-500'>Anas</span> - {year}&copy;</p>
         </footer>
     )
 }
