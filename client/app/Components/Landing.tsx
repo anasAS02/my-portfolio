840942f34b192c85d43cdfa9ba2c75b253b67ef8
Anas <110332632+anasAS02@users.noreply.github.com>
2024-11-13 18:44:07 +0200

    Update CV link

--- client/app/Components/Landing.tsx
+++ client/app/Components/Landing.tsx
@@ -22,7 +22,7 @@ export default function Landing(){
                 />
                 <p className='text-2xl max-md:text-sm'>I enjoy building new projects daily and constantly strive to learn more.</p>
                 <Flip>
-                    <Link href='https://drive.usercontent.google.com/download?id=1zN33NkCsdllMqdrORXE133xM9F2RAAzZ&export=download&authuser=0&confirm=t&uuid=0588b635-c47a-413a-bff5-8d840998ccce&at=APZUnTX52KQyG98-if5jhk-3eBMG:1723466269280'
+                    <Link href='https://drive.usercontent.google.com/download?id=15wyAswjfj4GhMD4r08v_6H3MZCTFU8Ig&export=download&authuser=0&confirm=t&uuid=8ca9d672-de3f-4e31-94c8-c108ac06156b&at=AENtkXZsDX3_3rP8I5SsA76IS1qz:1731516135132'
                     className='flex items-center gap-3 duration-300 dark:bg-white dark:hover:bg-yellow-500 dark:text-blue-500 dark:hover:text-white bg-blue-400 hover:bg-blue-300 text-white p-5 max-md:p-2 rounded-xl cursor-pointer'
                     >
                         Download CV
